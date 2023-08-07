230a78b342918b541d76bd679842364a61923533
TheCtkHoster <marshitarsh@gmail.com>
2023-08-07 16:12:14 +0530

    Update ping.js
    new things?!?!??!

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,8 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		return interaction.reply({ content: `🏓 Pong!`, ephemeral: true });
+	async execute(interaction, client) {
+		return interaction.reply({ content: `🏓 Pong! ${client.ws.ping}ms`, ephemeral: true });
 	},
 };
 
